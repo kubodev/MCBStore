14158fcfbc7054953779caaf228c3aab6c21ab28
kubodev <moisesmagna@gmail.com>
2019-09-08 11:58:17 -0300

    archivos modificados

---
+++ js/jquery.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    // mega-menu
+    $(".nav-pills a").mouseover(function() {
+        $('#v-pills-tab a[href="' + $(this).attr('href') + '"]').tab('show')
+    })
+
+    // stiky header 
+
+
+    $(window).scroll(function() {
+        var sticky = $('.sticky-top'),
+            scroll = $(window).scrollTop();
+
+        if (scroll >= 100) {
+            sticky.addClass('scroll');
+            $('#show-scroll').removeClass('d-none');
+        } else if (scroll <= 58) {
+            sticky.removeClass('scroll');
+            $('#show-scroll').addClass('d-none');
+        }
+    });
+
+})
+
+$(window).on('resize', function() {
+    activate();
+});
+
+function activate() {
+    var win = $(window).width();
+
+
+    if (win <= 578) {
+        $('.cantidad').hide();
+        $('.bg-white.bx-shadow').removeClass('sticky-top');
+    } else {
+        $('.cantidad').show();
+        $('.bg-white.bx-shadow').addClass('sticky-top');
+    }
+}
+activate();
+
+$('.productos .carousel').carousel({
+    interval: false
+})
